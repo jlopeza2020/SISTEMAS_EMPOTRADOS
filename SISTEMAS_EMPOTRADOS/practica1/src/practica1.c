cea3c8409d99396900c20161e1f6ff0a8b566255
jlopeza2020 <j.lopeza.2020@alumnos.urjc.es>
2022-09-28 10:53:07 +0200

    v2 p1

--- SISTEMAS_EMPOTRADOS/practica1/src/practica1.c
+++ SISTEMAS_EMPOTRADOS/practica1/src/practica1.c
@@ -4,17 +4,24 @@
 #include <time.h>
 #include <err.h>
 
+#ifdef DEBUG
+    #define DEBUG_PRINTF(...) printf("DEBUG: "__VA_ARGS__)
+#else
+    #define DEBUG_PRINTF(...)
+#endif
+
+
 #define NTHREADS 4
 #define ITERATIONS 5
 #define S2NS 1000000000
 #define ZERO_DOT_NINETYFIVE_S 905000000
-#define PERIOD 900000000L
+#define PERIOD 9000000000L
 #define ZERO_DOT_FIVE_S 398885000ULL
 
 
-struct timespec get_time(struct timespec begin, struct timespec end){
+void get_time(struct timespec *dif){
 
-    struct timespec dif;
+    struct timespec begin, end;
     volatile unsigned long long j;
     
     if (clock_gettime(CLOCK_MONOTONIC, &begin) != 0){
@@ -22,7 +29,7 @@ struct timespec get_time(struct timespec begin, struct timespec end){
         exit(EXIT_FAILURE);
     }
 
-    //I had to modify since 400000000ULL did not represent 0.5 seconds
+    // I had to modify since 400000000ULL did not represent 0.5 seconds
     for (j=0; j < ZERO_DOT_FIVE_S ; j++); 
 
     if (clock_gettime(CLOCK_MONOTONIC, &end) != 0){
@@ -32,31 +39,28 @@ struct timespec get_time(struct timespec begin, struct timespec end){
 
     // subtract seconds
     if(end.tv_sec < begin.tv_sec){
-        dif.tv_sec = begin.tv_sec - end.tv_sec;
+        dif->tv_sec = begin.tv_sec - end.tv_sec;
     }else{
-        dif.tv_sec = end.tv_sec - begin.tv_sec; 
+        dif->tv_sec = end.tv_sec - begin.tv_sec; 
     }
 
-    
-    // This is because the begin time is bigger than 5*10^8 ns and so,
-    // seconds in the end time is 1 value bigger.
-    if (dif.tv_sec == 1){
-        dif.tv_sec = dif.tv_sec - 1;
+    /* This is because the begin time is bigger than 5*10^8 ns and so,
+        seconds in the end time is 1 value bigger */
+    if (dif->tv_sec == 1){
+        dif->tv_sec = dif->tv_sec - 1;
     }
 
     // subtract nanoseconds
     if(end.tv_nsec  < begin.tv_nsec){
-        dif.tv_nsec = begin.tv_nsec - end.tv_nsec;
+        dif->tv_nsec = begin.tv_nsec - end.tv_nsec;
     }else{
-        dif.tv_nsec = end.tv_nsec - begin.tv_nsec;
+        dif->tv_nsec = end.tv_nsec - begin.tv_nsec;
     }
-
-    return dif;
 }
 
 void *thread_routine(void *ptr){
 
-    struct timespec begin, end, dif;
+    struct timespec begin_realt, dif;
     volatile unsigned long long j;
     char *message;
     int i;
@@ -66,23 +70,27 @@ void *thread_routine(void *ptr){
 
     for (i=1; i <= ITERATIONS; i++){ 
 
-        if (clock_gettime(CLOCK_REALTIME, &begin) != 0){
+        if (clock_gettime(CLOCK_REALTIME, &begin_realt) != 0){
             warnx ("error in clock get time");
             exit(EXIT_FAILURE);
         }
 
-        dif = get_time(begin, end);
+        get_time(&dif);
 
         cost = (double)dif.tv_sec + (double)dif.tv_nsec / S2NS;
 
         // round the function 
         if (dif.tv_sec > 0 || dif.tv_nsec > ZERO_DOT_NINETYFIVE_S){
             printf("[%ld.%ld] %s - Iteración %d : Coste=%.2f s (fallo temporal)\n", 
-                    begin.tv_sec, begin.tv_nsec, message, i, cost);
+                    begin_realt.tv_sec, begin_realt.tv_nsec, message, i, cost);
         }else{
             printf("[%ld.%ld] %s - Iteración %d : Coste=%.2f s\n", 
-                    begin.tv_sec, begin.tv_nsec, message, i, cost);
-            nanosleep((const struct timespec[]){{dif.tv_sec, PERIOD-dif.tv_nsec}}, NULL);
+                    begin_realt.tv_sec, begin_realt.tv_nsec, message, i, cost);
+            if (nanosleep((const struct timespec[]){{dif.tv_sec, PERIOD-dif.tv_nsec}}, NULL) < 0){
+                warnx ("error in clock get time");
+                exit(EXIT_FAILURE);
+            }
+            DEBUG_PRINTF("SLEEP: %ld \n",PERIOD-dif.tv_nsec);
         }
     }
 
